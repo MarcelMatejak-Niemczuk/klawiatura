6ac79437217aa3e4dec0aa73314c9a7631133151
MarcelMatejak-Niemczuk <klocekkube@gmail.com>
2025-05-16 08:25:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function obliczRate() {
     var checkbox1 = document.getElementById("kurs1");
     var checkbox2 = document.getElementById("kurs2");
-    var raty = parseInt(document.getElementById("raty").value);
+    var ratyInput = document.getElementById("raty");
     var miasto = document.getElementById("lista").value;
+    var wynikElement = document.getElementById("wynik");
 
     var suma = 0;
 
+    if (!checkbox1.checked && !checkbox2.checked) {
+        wynikElement.innerText = "Wybierz jeden z kursów";
+        return;
+    }
+
+    var raty = parseInt(ratyInput.value);
+    if (isNaN(raty) || raty < 2) {
+        wynikElement.innerText = "Wpisz conajmniej 2";
+        return;
+    }
 
     if (checkbox1.checked) {
         suma += 5000;
@@ -15,11 +26,8 @@ function obliczRate() {
         suma += 3000;
     }
 
-    var rata = suma / raty;
-
-    rata = Math.round(rata);
+    var rata = Math.round(suma / raty);
 
     var wynik = 'Kurs odbędzie się w ' + miasto + '. Koszt całkowity: ' + suma + ' zł. Płacisz ' + raty + ' rat po ' + rata + ' zł.';
-
-    document.getElementById("wynik").innerText = wynik;
+    wynikElement.innerText = wynik;
 }
